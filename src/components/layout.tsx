a7e137187035c7e9f548a1f59fc219a184f52c40
Lev Kowalski <levkowalski@gmail.com>
2019-12-29 22:40:40 +0100

    wip

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,6 +7,7 @@ import Header from './Header'
 
 import './layout.css'
 import Footer from './Footer'
+import IntlProvider from '../intl/IntlContext'
 
 type Props = {
   children: React.ReactNode
@@ -34,18 +35,20 @@ const Layout = ({ children }: Props) => (
         >
           <html lang="en" />
         </Helmet>
-        <Header />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-          <Footer />
-        </div>
+        <IntlProvider>
+          <Header />
+          <div
+            style={{
+              margin: '0 auto',
+              maxWidth: 960,
+              padding: '0px 1.0875rem 1.45rem',
+              paddingTop: 0,
+            }}
+          >
+            {children}
+            <Footer />
+          </div>
+        </IntlProvider>
       </>
     )}
   />
