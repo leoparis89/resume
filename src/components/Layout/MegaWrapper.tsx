d332ac6afbd358624b847fdf20fa7cbf881be4cf
Lev Kowalski <levkowalski@gmail.com>
2021-05-08 21:33:18 +0200

    wip

---
+++ src/components/Layout/MegaWrapper.tsx
@@ -0,0 +1,26 @@
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { ThemeProvider } from '@material-ui/core/styles'
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import theme from '../../theme'
+
+export default function MegaWrapper(props: any) {
+  return (
+    <React.Fragment>
+      <Helmet>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        {/* <link
+          href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap"
+          rel="stylesheet"
+        /> */}
+      </Helmet>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {props.children}
+      </ThemeProvider>
+    </React.Fragment>
+  )
+}
