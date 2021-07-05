ace4efc265bf0e005583917988cf8780c832b524
Lev Kowalski <levkowalski@gmail.com>
2021-07-05 19:33:54 +0200

    stupid path case bug

--- src/pages/education.tsx
+++ src/pages/education.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Education from '../components/content/Education'
-import Layout from '../components/layout/Layout'
+import Layout from '../components/bar/Layout'
 
 const EducationPage = () => (
   <Layout>
--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Work from '../components/content/ProfessionalExperience'
-import Layout from '../components/layout/Layout'
+import Layout from '../components/bar/Layout'
 
 const WorkPage = () => (
   <Layout>
