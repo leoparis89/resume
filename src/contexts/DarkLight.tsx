6a13e22862849de2311a3591deea773dbf0550c6
Lev Kowalski <levkowalski@gmail.com>
2021-08-25 17:14:49 +0200

    change white theme

--- src/contexts/DarkLight.tsx
+++ src/contexts/DarkLight.tsx
@@ -1,9 +1,4 @@
-import {
-  colors,
-  createMuiTheme,
-  CssBaseline,
-  ThemeProvider,
-} from '@material-ui/core'
+import { createMuiTheme, CssBaseline, ThemeProvider } from '@material-ui/core'
 import React, { useContext, useState } from 'react'
 
 export enum Theme {
@@ -62,4 +57,4 @@ const makeBg = (col1, col2) => `linear-gradient(0.3turn, ${col1}, ${col2})`
 
 export const DARK_BG = makeBg('rgb(24 37 42)', 'rgb(71 65 120)')
 
-export const LIGHT_BG = makeBg('#ffff', colors.lightBlue[100])
+export const LIGHT_BG = makeBg('#ffff', '#ffff')
