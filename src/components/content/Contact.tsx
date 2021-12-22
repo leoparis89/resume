711eeafa4014201571e0a0a0aa1c8e6476590fbe
Lev Kowalski <lkowalski@accengage.com>
2021-12-22 15:09:27 +0100

    wip

--- src/components/content/Contact.tsx
+++ src/components/content/Contact.tsx
@@ -1,3 +1,5 @@
+import { Typography } from '@material-ui/core'
+import { styled } from '@material-ui/styles'
 import React from 'react'
 import { useLang } from '../../contexts/IntlContext'
 import * as wording from '../../wording'
@@ -14,14 +16,27 @@ const Contact = () => {
         <wording.Translate phrase="CONTACT" />
       </PageTitle>
 
-      <ul style={{ textDecoration: 'none' }}>
-        <li style={{ textDecoration: 'none' }}>{email}</li>
-        <li>{city}</li>
-        <li>{github}</li>
-        <li>{linkedin}</li>
+      <ul style={{ listStyleType: 'none' }}>
+        <MyLi>{email}</MyLi>
+        <MyLi>{city}</MyLi>
+        <MyLi>{github}</MyLi>
+        <MyLi>{linkedin}</MyLi>
       </ul>
     </div>
   )
 }
 
+const MyLi = styled('li')(({ theme }) => {
+  return { fontSize: theme.spacing(2) }
+})
+
+const LI = (props) => (
+  <li>
+    {' '}
+    <Typography variant="button" {...props}>
+      {' '}
+      hello
+    </Typography>
+  </li>
+)
 export default Contact
