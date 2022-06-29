830abad2e25f179e7af5ba77db26b0acc7722fd7
Lev Kowalski <levkowalski@gmail.com>
2022-06-29 23:42:48 +0200

    Implement lang param

--- src/contexts/IntlProvider.tsx
+++ src/contexts/IntlProvider.tsx
@@ -16,7 +16,13 @@ export const IntlContext = React.createContext<{
 export const useLang = () => useContext(IntlContext).lang
 
 const IntlProvider = (props) => {
-  const [lang, setLang] = useState<NodeLocale>(NodeLocale.FR)
+  const queryString = window.location.search
+  const urlParams = new URLSearchParams(queryString)
+  const langParam = urlParams.get('lang')
+
+  const [lang, setLang] = useState<NodeLocale>(
+    langParam === 'en' ? NodeLocale.EN : NodeLocale.FR
+  )
 
   const toggleLang = () => {
     const newLang = lang === NodeLocale.FR ? NodeLocale.EN : NodeLocale.FR
