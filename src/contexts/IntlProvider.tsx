fd40e0f9c78fa5434cc65b17c572b0fe373fb064
Lev Kowalski <levkowalski@gmail.com>
2022-07-05 15:21:29 +0200

    fix initial lang

--- src/contexts/IntlProvider.tsx
+++ src/contexts/IntlProvider.tsx
@@ -13,16 +13,25 @@ export const IntlContext = React.createContext<{
   toggleLang: () => {},
 })
 
+const getLang = (langParam: string) => {
+  if (/^fr\b/.test(langParam)) {
+    return NodeLocale.FR
+  }
+
+  return NodeLocale.EN
+}
+
 export const useLang = () => useContext(IntlContext).lang
 
 const IntlProvider = (props) => {
   const queryString = window.location.search
   const urlParams = new URLSearchParams(queryString)
   const langParam = urlParams.get('lang')
+  const langFromBrowser = window.navigator.language
 
-  const [lang, setLang] = useState<NodeLocale>(
-    langParam === 'en' ? NodeLocale.EN : NodeLocale.FR
-  )
+  const initialLang = getLang(langParam || langFromBrowser)
+
+  const [lang, setLang] = useState<NodeLocale>(initialLang)
 
   const toggleLang = () => {
     const newLang = lang === NodeLocale.FR ? NodeLocale.EN : NodeLocale.FR
